c9448094c05ec494983e0772501f44cf0adc6223
Rubikon4 <vazintag@yandex.ru>
2024-11-13 17:55:19 +0300

    First commit

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function showTasks() {
         for  (var i = 0; i < stroageSize; i++); {
             var key = localStorage.key(i);
             if (key.indexOf(mask) == 0) {
-                $('<li><li>').addClass('tdItem')
+                $('<li></li>').addClass('tdItem')
                 .attr('data-itemid', key)
                 .text(localStorage.getItem(key))
                 .appendTo(list);
@@ -15,3 +15,25 @@ function showTasks() {
         }
     }
 }
+
+showTasks();
+
+$('#tdlApp input').on('keydown', function(e) {
+    if (e.keyCode != 13) return;
+    var str = e.target.value;
+    e.target.value = "";
+    if (str.length > 0) {
+        var number_Id = 0;
+        list.children().each(function(index, el) {
+            var element_Id = $(el).attr('data-itemid').slice(4);
+            if (element_Id >number_Id)
+                number_Id = element_Id;
+        })
+        number_Id++;
+        localStorage.setItem(mask + number_Id, str);
+        $('<li></li>').addClass('tdItem')
+            .attr('data-itemid', mask +number_Id)
+            .text(str).appendTo(list);
+    }
+});
+
